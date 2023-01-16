47b80f32072c6158e240f9bb67217de52c952554
kyungmin <antcjswo@kakao.com>
2023-01-16 21:55:04 +0900

    23.01.16 - patch, delete, image

--- src/main/java/com/korit/library/service/BookService.java
+++ src/main/java/com/korit/library/service/BookService.java
@@ -3,19 +3,25 @@ package com.korit.library.service;
 
 import com.korit.library.exception.CustomValidationException;
 import com.korit.library.repository.BookRepository;
-import com.korit.library.web.dto.BookMstDto;
-import com.korit.library.web.dto.BookReqDto;
-import com.korit.library.web.dto.CategoryDto;
-import com.korit.library.web.dto.SearchReqDto;
+import com.korit.library.web.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 @Service
 public class BookService {
+
+    @Value("${file.path}") //application.yml 안의 (표현식)경로를 들고온다.
+    private String filePath;
+
 
     @Autowired
     private BookRepository bookRepository;
@@ -49,5 +55,54 @@ public class BookService {
 
     public void modifyBook(BookReqDto bookReqDto){
         bookRepository.updateBookByBookCode(bookReqDto);
+    }
+    public void maintainModifyBook(BookReqDto bookReqDto){
+        bookRepository.maintainUpdateBookByBookCode(bookReqDto);
+    }
+    public void deleteBook(String bookCode){
+        bookRepository.deleteBookByBookCode(bookCode);
+    }
+
+    public void registerBookImages(String bookCode, List<MultipartFile> files){
+        if(files.size() < 1) {
+            Map<String, String> errorMap = new HashMap<String, String>();
+            errorMap.put("files", "이미지를 선택하세요.");
+
+            throw new CustomValidationException(errorMap);
+        }
+
+        List<BookImageDto> bookImageDtos = new ArrayList<BookImageDto>();
+        files.forEach(file -> {
+            String originFileName = file.getOriginalFilename();
+            String extension = originFileName.substring(originFileName.lastIndexOf(".")); //substring : 뒤에서 부터 '.'까지 잘라라 ex) .png
+            String tempFileName = UUID.randomUUID().toString().replaceAll("-","") + extension; //UUID 임시로 id잡아준다 => 가짜이름 + extension(.png)
+
+            Path uploadPath = Paths.get(filePath + "/book/" + tempFileName);
+            //filePath + "/book/" + tempFileName 이 경로로 uploadPath에 지정?저장?
+            //path, paths : java.nio.file 로 import
+
+            File f = new File(filePath + "/book");
+
+            if(!f.exists()){ // ->경로유무 확인.
+                f.mkdirs();
+            }// => 경로가 없으면 경로 생성해라
+
+            try {  //try패치....?
+                Files.write(uploadPath, file.getBytes()); //위에서 들고온거 써라...
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            BookImageDto bookImageDto = BookImageDto.builder()
+                    .bookCode(bookCode)
+                    .saveName(tempFileName)
+                    .originName(originFileName)
+                    .build();
+            bookImageDtos.add(bookImageDto);
+
+        });
+
+        bookRepository.registerBookImages(bookImageDtos);
+
     }
 }
--- src/main/java/com/korit/library/web/api/admin/BookApi.java
+++ src/main/java/com/korit/library/web/api/admin/BookApi.java
@@ -6,12 +6,14 @@ import com.korit.library.aop.annotation.ValidAspect;
 import com.korit.library.service.BookService;
 import com.korit.library.web.dto.*;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -54,11 +56,47 @@ public class BookApi {
     //기존자료 수정
     @ParamsAspect
     @ValidAspect
-    @PutMapping("/book/{bookCode}") //PatchMapping : 비어있는 부분에 대해서는 기존값을 그대로 갖고 온다 + 수정된 애들만 업데이트  PutMapping : 비어있는부분을 null 값으로 대체 해버린다.
+    @PatchMapping("/book/{bookCode}") //PatchMapping : 비어있는 부분에 대해서는 기존값을 그대로 갖고 온다 + 수정된 애들만 업데이트  PutMapping : 비어있는부분을 null 값으로 대체 해버린다.
+    public ResponseEntity<CMRespDto<?>> MaintainModifyBook(@PathVariable String bookCode, @Valid @RequestBody BookReqDto bookReqDto, BindingResult bindingResult){
+        bookService.maintainModifyBook(bookReqDto);
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+    @ParamsAspect
+    @ValidAspect
+    @PutMapping("/book/{bookCode}")
     public ResponseEntity<CMRespDto<?>> modifyBook(@PathVariable String bookCode, @Valid @RequestBody BookReqDto bookReqDto, BindingResult bindingResult){
         bookService.modifyBook(bookReqDto);
         return ResponseEntity
                 .ok()
                 .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
     }
+
+
+
+    @ParamsAspect
+    @DeleteMapping("/book/{bookCode}")
+    public ResponseEntity<CMRespDto<?>> deleteBook(@PathVariable String bookCode){
+        bookService.deleteBook(bookCode);
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+
+    @ParamsAspect
+    @PostMapping("/book/{bookCode}/images")
+    public ResponseEntity<CMRespDto<?>> registerBookImg(@PathVariable String bookCode, @RequestPart List<MultipartFile> files){
+        MultipartFile file = files.get(0);
+        System.out.println(file.getOriginalFilename());
+
+        bookService.registerBookImages(bookCode, files);
+
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+
+
+
 }
