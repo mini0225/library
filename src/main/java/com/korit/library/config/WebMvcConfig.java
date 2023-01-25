08b3f71c6de9ef1a7af593a31185b19aa0760185
kyungmin <antcjswo@kakao.com>
2023-01-25 21:51:03 +0900

    23.01.25 카테고리 도서검색 + 대여가능여부 표시

--- src/main/java/com/korit/library/config/WebMvcConfig.java
+++ src/main/java/com/korit/library/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.korit.library.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
@@ -21,6 +22,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${file.path}")
     private String filePath;
+
+    //http://127.0.0.1:5500 서버에서 들어오는 모든 요청("/**")에 대해서 CORS 오류 해결
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500/");
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         WebMvcConfigurer.super.addResourceHandlers(registry);
